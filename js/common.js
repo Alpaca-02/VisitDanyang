5d5df555a4f5cc42a408ae918df0b56fc663ba35
Son <sky__32@naver.com>
2022-10-17 13:47:40 +0900

    섹션스크롤 구현

--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
 $(function(){
-    // 섹션스크롤
+    //// 섹션스크롤 영역
+    // 브라우저 기본 스크롤 제한
+    window.addEventListener("wheel", function(e){
+        e.preventDefault();
+    },{passive : false});
+
+
+var mHtml = $("html");
+var page = 1;
+
+mHtml.animate({scrollTop : 0},10);
+
+
+    $(window).on("wheel", function(e) {
+        if(mHtml.is(":animated")) return;
+        if(e.originalEvent.deltaY > 0) {
+            if(page == 4) return;
+            page++;
+        } else if(e.originalEvent.deltaY < 0) {
+            if(page == 1) return;
+            page--;
+        }
+        var posTop =(page-1) * $(window).height();
+        mHtml.animate({scrollTop : posTop});
+    })
 
     // 스크롤 위치 감지
     window.addEventListener('scroll',()=> {
@@ -15,8 +39,6 @@ $(function(){
     });
 
     
-
-
 
 
 
